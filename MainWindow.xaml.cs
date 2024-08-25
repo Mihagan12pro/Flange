d7a74d4a8ec0ebf4e402b4481a0bbfdb5acbd38f
Mihagan12pro <mikhail.kargin.05@mail.ru>
2024-08-25 11:52:02 +0300

    The FlangeModel class was created.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using Flange.Model;
 namespace Flange
 {
     /// <summary>
@@ -23,6 +23,12 @@ namespace Flange
         public MainWindow()
         {
             InitializeComponent();
+
+            
+
+           
+
+            DataContext = new FlangeModel();
         }
     }
 }
---
+++ Model/FlangeModel.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Media3D;
+
+namespace Flange.Model
+{
+    internal class FlangeModel : DependencyObject
+    {
+        private struct FlangeTypeStorage
+        {
+            public readonly ObservableCollection<string> FlangeTypesCBItems { get;  set; }
+        }
+
+        private readonly string SimpleFlangeType;
+
+        
+
+
+        private bool dVisible ;
+        public bool DVisible
+        {
+            get
+            {
+                return dVisible;
+            }
+            set
+            {
+                dVisible = value;
+            }
+           
+        }
+
+        private string flangeTypeCrl;
+        public string FlangeTypeCrl
+        {  
+            get
+            {
+                return flangeTypeCrl;
+            }
+            set
+            {
+                FlangeTypeCrl = value;
+
+                switch(FlangeTypeCrl)
+                {
+                    case FlangeTypesCBItems[0]:
+                        break;
+                }
+            }
+        }
+
+        public FlangeModel()
+        {
+            dVisible = false;
+
+           
+
+            
+
+            FlangeTypesCBItems = new ObservableCollection<string>
+            {
+                "Фланец",
+                "Свободный фланец",
+                "Плоский фланец"
+            };
+            flangeTypeCrl = FlangeTypesCBItems[0] ;
+        }
+        
+    }
+}
