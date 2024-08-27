ece78f8bb0baa10ddf57c1b02552c54045f69d81
Mihagan12pro <mikhail.kargin.05@mail.ru>
2024-08-27 10:53:23 +0300

    The abstract class Kompas and its child classes were created.

---
+++ Kompas/Kompas.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Kompas6API5;
+namespace Flange.Kompas
+{
+    internal abstract class Kompas
+    {
+        protected KompasObject kompasObject;
+        public KompasObject KompasObject
+        {
+            get
+            {
+                return kompasObject;
+            }
+            set
+            {
+                kompasObject = value;
+            }
+        }
+
+
+
+    }
+}
