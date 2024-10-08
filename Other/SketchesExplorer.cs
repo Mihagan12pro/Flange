689b920a52a0da75155f0b8de244df980443424d
Mihagan12pro <mikhail.kargin.05@mail.ru>
2024-10-08 08:38:25 +0300

    The SketchesExplorer class eas created.

---
+++ Other/SketchesExplorer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using PathExplorerLibrary;
+namespace Flange.Other
+{
+    internal class SketchesExplorer : AbstractExplorer
+    {
+        public SketchesExplorer(PathMaster pathMaster)  : base(pathMaster)
+        {
+            
+        }
+        protected override void UniquePaths()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
