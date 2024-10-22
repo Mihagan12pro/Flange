00deb6b75731b3d539134dbc3ffcf2487ade8cf1
Mihagan12pro <mikhail.kargin.05@mail.ru>
2024-10-22 17:57:08 +0300

    The abstract Table class was created.

---
+++ ViewModel/Tables/Table.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Flange.Other;
+using Flange.Model;
+using System.Collections.ObjectModel;
+using System.Linq.Expressions;
+using Flange.Databases.Classes.Standart.Data_storages;
+namespace Flange.ViewModel.Tables
+{
+    internal abstract class Table
+    {
+        // public readonly string 
+
+        protected readonly string database;
+
+        protected  ObservableCollection<DataStorage>data;
+
+        public ObservableCollection<DataStorage> Data 
+        { 
+            get
+            {
+                return data;
+            }
+            protected set 
+            {
+                data = value;
+            } 
+        }
+
+        public Table()
+        {
+            database = MainExplorer.DataBaseExpl.DefaultSizesFullName;
+        }
+    }
+    class FreeSimpleTable:Table
+    {
+
+    }
+}
