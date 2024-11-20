e3b520c9efe588e5c01079a0a23762db5e83e343
Mihagan12pro <mikhail.kargin.05@mail.ru>
2024-11-20 08:15:09 +0300

    The CustomToolBar class was created. The "WPF override folder" was created.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -30,47 +30,11 @@ namespace Flange
 
 
 
-
-        //            < DataGrid Canvas.Left = "{Binding TableOffsetX.Left}" Canvas.Right = "100" ColumnWidth = "{Binding ColumnWidth}" Grid.Column = "0"  CanUserSortColumns = "False" BorderBrush = "White" Width = "{Binding  TableWidth}" IsReadOnly = "True"  CanUserReorderColumns = "False"   CanUserAddRows = "False"  SelectedIndex = "{Binding TableRowIndex}"   SelectionMode = "Single"  Background = "White"  ItemsSource = "{Binding TableData.Data}"     Name = "TableDataGr" Grid.ColumnSpan = "2" Margin = "230,39,162,83" >
-
-        //    < DataGrid.Columns >
-
-        //    </ DataGrid.Columns >
-        //</ DataGrid >
-
-
-            SizeD1Lbl.MaxWidth = SizeD1Lbl.Width;
-            SizeDLbl.MaxWidth = SizeDLbl.Width;
-            SizeD2Lbl.MaxWidth = SizeD2Lbl.Width;
-            SizeHLbl.MaxWidth = SizeHLbl.Width;
-
-
             DataContext = new FlangeViewModel();
-
-
-
-            MainCanvas.SizeChanged += MainCanvas_SizeChanged;
-
-            MouseDown += MainWindow_MouseDown;
-
-          
-        }
-
-     
-
-        private void MainWindow_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-           // TableDataGr.SelectedIndex = -1;
             
         }
 
-        private void MainCanvas_SizeChanged(object sender, SizeChangedEventArgs e)
-        {
-            if (DataContext is FlangeViewModel flangeModel)
-            {
-                flangeModel.UpdateTableWidth(e.NewSize.Width);
-            }
-        }
+
 
 
     }
