c1fe5ffb0506759358389c49fbddefdef078033e
Mihagan12pro <mikhail.kargin.05@mail.ru>
2025-01-16 20:45:19 +0300

    RadioMenuItem and  RadioMenuItemBox classes were created.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data.Entity;
 using System.Data.SqlTypes;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -16,6 +17,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 //using Flange.Databases.Classes.Standart;
 using Flange.Model;
+using Flange.Other.WPF_override;
 namespace Flange
 {
     /// <summary>
@@ -28,7 +30,16 @@ namespace Flange
         {
             InitializeComponent();
 
+            RadioMenuItem kompas3DItem = new RadioMenuItem();
+            kompas3DItem.Name = "Kompas3DMenuItem";
+            kompas3DItem.Header = "Компас-3D";
 
+            RadioMenuItem nanoCADItem = new RadioMenuItem();
+            kompas3DItem.Name = "nanoCadMenuItem";
+            kompas3DItem.Header = "nanoCAD";
+
+            ChooseCADMenuItem.InsertChild(kompas3DItem);
+            ChooseCADMenuItem.InsertChild(nanoCADItem);
 
             DataContext = new FlangeViewModel();
             
---
+++ Other/WPF override/RadioMenuItem.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+namespace Flange.Other.WPF_override
+{
+    internal class RadioMenuItem : MenuItem
+    {
+        private int id;
+
+        private RadioMenuItemBox father;
+
+        private bool inserted = false;
+
+        public void Insert(RadioMenuItemBox _father,int _id)
+        {
+            if (!inserted)
+            {
+                id = _id;
+                father = _father;
+                inserted = true;
+            }
+        }
+
+        public RadioMenuItem()
+        {
+            IsCheckable = true;
+        }
+    }
+
+    internal class RadioMenuItemBox: MenuItem
+    {
+        
+        public RadioMenuItemBox()
+        {
+        
+        }
+
+        protected override void AddChild(object value)
+        {
+            RadioMenuItem child = value as RadioMenuItem;
+
+            child.Insert(this,Items.Count);
+
+            base.AddChild(child);
+        }
+        public void InsertChild(RadioMenuItem menuItem)
+        {
+            AddChild(menuItem);
+        }
+    }
+   
+}
