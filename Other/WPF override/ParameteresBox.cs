1c6550bd6f4f6e56f0ada4cd70ba63779e050b45
Mihagan12pro <mikhail.kargin.05@mail.ru>
2025-01-25 13:26:04 +0300

    A FormDictionary was added to the project.

---
+++ Other/WPF override/ParameteresBox.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flange.Other.WPF_override
+{
+    internal class ParameteresBox
+    {
+    }
+}
